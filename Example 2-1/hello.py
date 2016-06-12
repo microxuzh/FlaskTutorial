cc2bc8b99cb4c656b9b16e9103b7f17143137f65
microxuzh <microxu@yeah.net>
2016-06-12 23:48:39 +0800

    Flask application with a dynamic route

--- Example 2-1/hello.py
+++ Example 2-1/hello.py
@@ -7,6 +7,11 @@ def index():
 	return '<h1>Hello World!</h1>'
 
 
+@app.route('/user/<name>')
+def user(name):
+	return '<h1>Hello, %s!</h1>' % name
+
+
 if __name__ == '__main__':
 	app.run(debug=True)                       # app.run()  default listen on localhost:5000
 
