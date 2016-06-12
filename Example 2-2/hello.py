82c8c576b755a5e8e1561f4c33ae90f27e79b9ce
microxuzh <microxu@yeah.net>
2016-06-13 00:08:49 +0800

    import request package

--- Example 2-2/hello.py
+++ Example 2-2/hello.py
@@ -1,5 +1,5 @@
 # Flask application with a dynamic route
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
@@ -11,6 +11,11 @@ def index():
 @app.route('/user/<name>')
 def user(name):
 	return '<h1>Hello, %s!</h1>' % name
+
+@app.route('/agent')
+def agent():
+	user_agent = request.headers.get('User-Agent')
+	return '<p>Your browser is %s</p>' % user_agent
 
 if __name__ == '__main__':
 	app.run(debug=True)       
